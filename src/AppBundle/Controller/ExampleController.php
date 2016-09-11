e56c1004023967a3be9945dfa45cd93f17529aae
sameddin <sameddin.pasizade@gmail.com>
2016-09-11 20:45:17 +0400

    Add task creation on example route

--- src/AppBundle/Controller/ExampleController.php
+++ src/AppBundle/Controller/ExampleController.php
@@ -2,8 +2,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\TodoList;
+use AppBundle\Form\Type\TodoListAddType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExampleController extends Controller
 {
@@ -16,8 +19,33 @@ class ExampleController extends Controller
             ->getRepository('AppBundle:TodoList')
             ->findAll();
 
+        $task = new TodoList();
+        $form = $this->createForm(TodoListAddType::class, $task, [
+            'action' => $this->generateUrl('task.add'),
+            'method' => 'POST',
+        ]);
+
         return $this->render('example.html.twig', [
             'tasks' => $tasks,
+            'form'  => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/example/add", name="task.add")
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function addAction(Request $request)
+    {
+        $task = new TodoList();
+        $form = $this->createForm(TodoListAddType::class, $task);
+        $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($task);
+        $em->flush();
+
+        return $this->redirectToRoute('example');
     }
 }
---
+++ src/AppBundle/Form/Type/TodoListAddType.php
@@ -0,0 +1,37 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\TodoList;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TodoListAddType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('task', TextType::class, [
+                'label' => 'common.task',
+                'attr'  => [
+                    'placeholder' => 'common.task.placeholder',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'common.submit',
+                'attr'  => [
+                    'class' => 'btn-primary',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\TodoList',
+        ]);
+    }
+}
