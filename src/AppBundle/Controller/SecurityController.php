83082281b7f48258a0ecf6d3ee03f4fe6ecb5b85
sameddin <sameddin.pasizade@gmail.com>
2016-09-25 23:47:41 +0400

    Implement Login Form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
