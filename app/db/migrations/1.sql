849fa7d5f4f9ae45973ed751694753b67a4fd29d
sameddin <sameddin.pasizade@gmail.com>
2016-09-25 23:02:28 +0400

    Create User Entity

--- app/db/migrations/1.sql
+++ app/db/migrations/1.sql
@@ -5,3 +5,16 @@ CREATE TABLE todo_list
 
   PRIMARY KEY (id)
 );
+
+CREATE TABLE "user"
+(
+  id         bigserial NOT NULL,
+  nick_name  varchar   NOT NULL,
+  email      varchar   NOT NULL,
+  password   varchar   NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX ON "user" (lower(nick_name));
+CREATE UNIQUE INDEX ON "user" (lower(email));
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,159 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * @Entity()
+ * @Table(name="`user`")
+ * @UniqueEntity(fields="email", message="Email already taken")
+ * @UniqueEntity(fields="nickName", message="Nickname already taken")
+ */
+class User implements UserInterface
+{
+    /**
+     * @Column(type="bigint")
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", length=255, unique=true)
+     * @NotBlank()
+     */
+    private $nickName;
+
+    /**
+     * @Column(type="string", length=255, unique=true)
+     * @NotBlank()
+     * @Email()
+     *
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @Column(type="string", length=64)
+     *
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @NotBlank()
+     * @Length(max=4096)
+     *
+     * @var string
+     */
+    private $plainPassword;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNickName()
+    {
+        return $this->nickName;
+    }
+
+    /**
+     * @param string $nickName
+     */
+    public function setNickName($nickName)
+    {
+        $this->nickName = $nickName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
