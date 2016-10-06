52911e5ffd2947cce5eec12f62ea048fd9067fbf
sameddin <sameddin.pasizade@gmail.com>
2016-10-06 22:58:17 +0400

    Implement a Simple Registration Form

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,37 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Form\Type\UserType;
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+            return $this->render('register.html.twig', [
+                'form' => $form->createView(),
+            ]);
+    }
+}
---
+++ src/AppBundle/Form/Type/UserType.php
@@ -0,0 +1,56 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nickName', TextType::class, [
+                'label' => 'common.username',
+                'attr' => [
+                    'placeholder' => 'common.username',
+                    'autofocus' => true,
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'common.email',
+                'attr' => [
+                    'placeholder' => 'common.email',
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'common.password',
+                    'attr' => [
+                        'placeholder' => 'common.password',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'common.password_repeat',
+                    'attr' => [
+                        'placeholder' => 'common.password_repeat',
+                    ],
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\User',
+        ]);
+    }
+}
